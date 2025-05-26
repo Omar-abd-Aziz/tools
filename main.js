4822b0c9148f816a667be7cee87ac0fc06f5ede1
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2025-05-26 23:55:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,34 +4,52 @@
 let AllToolsObject = [{
     ToolName: "easy-pay-tax",
     ToolLink: "https://omar-abd-aziz.github.io/easy-pay/",
-},{
+},
+{
     ToolName: "Short-Link",
     ToolLink: "https://omar-abd-aziz.github.io/Short-Link/",
-},{
+},
+{
     ToolName: "File-Upload",
     ToolLink: "https://omar-abd-aziz.github.io/File-Uploaded-By-Elhalwany/",
-},{
+},
+{
+    ToolName: "مصفوفة الأولويات - الأشياء الأولى أولاً",
+    ToolLink: "https://firebasestorage.googleapis.com/v0/b/pagebuilder-6d4aa.appspot.com/o/webpage_1748290486114.html?alt=media&token=00cb1395-7b90-41f0-977b-111425af251a",
+},
+{
+    ToolName: "نظام نقاط البيع",
+    ToolLink: "https://firebasestorage.googleapis.com/v0/b/pagebuilder-6d4aa.appspot.com/o/webpage_1748291406035.html?alt=media&token=2a5b7336-e458-416e-8122-815569cebfb3",
+},
+{
     ToolName: "Notes",
     ToolLink: "https://omar-abd-aziz.github.io/Notes/",
-},{
+},
+{
     ToolName: "Notes-Online",
     ToolLink: "https://omar-abd-aziz.github.io/Notes-Online/index.html",
-},{
+},
+{
   ToolName: "Max-Eg",
   ToolLink: "https://omar-abd-aziz.github.io/Max-Eg/index.html",
-},{
+},
+{
   ToolName: "Css-Bg-Gradians",
   ToolLink: "https://www.gradientmagic.com/browse",
-},{
+},
+{
   ToolName: "CSS Sections",
   ToolLink: "https://wweb.dev/resources/css-separator-generator",
-},{
+},
+{
   ToolName: "CSS Box Shadow",
   ToolLink: "https://getcssscan.com/css-box-shadow-examples",
-},{
+},
+{
   ToolName: "Web To Apk",
   ToolLink: "https://volt.build/upload/",
-},{
+},
+{
   ToolName: "Fancy-Border-Radius",
   ToolLink: "https://9elements.github.io/fancy-border-radius/#89.100.22.81--.",
 },
